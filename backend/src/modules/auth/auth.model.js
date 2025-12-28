adcd4284f20a78259c5c1129033bec4ed5a30c12
Abdullah Khaver <muhammadabdullahkhaver@gmail.com>
2025-12-28 14:02:18 -0500

    bugs

--- backend/src/modules/auth/auth.model.js
+++ backend/src/modules/auth/auth.model.js
@@ -15,7 +15,7 @@ const AuthSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Password is required'],
       minlength: [6, 'Password must be at least 6 characters long'],
-      select: false, // IMPORTANT Improvement
+      select: false, 
     },
   },
   { timestamps: true },
